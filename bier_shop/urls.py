dfa6c16abd7aaa9dbd1517ce02abd75d88aa1a44
Remi BESNARD <remi@besnard-consulting.com>
2022-06-25 12:06:17 -0700

    Cancel Button Form working on Bier Company card.

--- bier_shop/urls.py
+++ bier_shop/urls.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.urls import path
 
 from bier_shop.views import BierCompanyListView, AddBierCompanyView
@@ -5,4 +6,5 @@ from bier_shop.views import BierCompanyListView, AddBierCompanyView
 urlpatterns = [
     path("", BierCompanyListView.as_view(), name="bier-company-list"),
     path("add-bier-company", AddBierCompanyView.as_view(), name="add-bier-company"),
+    path("cancel-form", lambda request: HttpResponse(), name="cancel-form"),
 ]
