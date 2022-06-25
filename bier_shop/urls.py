da8d092e20822d7116a738f581dcb3d94cead702
Remi BESNARD <remi@besnard-consulting.com>
2022-06-25 10:33:15 -0700

    Add Bier Company Form Component added.

--- bier_shop/urls.py
+++ bier_shop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from bier_shop.views import BierCompanyListView
+from bier_shop.views import BierCompanyListView, AddBierCompanyView
 
 urlpatterns = [
     path("", BierCompanyListView.as_view(), name="bier-company-list"),
+    path("add-bier-company", AddBierCompanyView.as_view(), name="add-bier-company"),
 ]
--- bier_shop/views.py
+++ bier_shop/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render
+from django.views import View
 from django.views.generic import ListView
 
 from bier_shop.models import BierCompany
@@ -6,3 +8,8 @@ from bier_shop.models import BierCompany
 class BierCompanyListView(ListView):
     model = BierCompany
     template_name = "bier_shop/bier_company_list.html"
+
+
+class AddBierCompanyView(View):
+    def get(self, request):
+        return render(request, "bier_shop/components/add_bier_company_form.html")
