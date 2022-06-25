2b32528bdb0a461b96bd154e20e6b87f98d04fad
Remi BESNARD <remi@besnard-consulting.com>
2022-06-25 06:10:13 -0700

    Black enforced.

--- bier_shop/migrations/0001_initial.py
+++ bier_shop/migrations/0001_initial.py
@@ -9,29 +9,65 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='BierCompany',
+            name="BierCompany",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('logo', models.ImageField(upload_to='')),
-                ('country', django_countries.fields.CountryField(max_length=2)),
-                ('description', models.TextField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                ("logo", models.ImageField(upload_to="")),
+                ("country", django_countries.fields.CountryField(max_length=2)),
+                ("description", models.TextField()),
             ],
         ),
         migrations.CreateModel(
-            name='Bier',
+            name="Bier",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('image', models.ImageField(upload_to='')),
-                ('bier_type', models.CharField(choices=[('Pilsner', 'Pilsner'), ('Dark Lager', 'Dark Lager'), ('German Bock', 'German Bock'), ('Brown Ale', 'Brown Ale'), ('Pale Ale', 'Pale Ale'), ('India Pale Ale', 'Indian Pale Ale'), ('Porter', 'Porter'), ('Stout', 'Stout')], max_length=100)),
-                ('description', models.TextField()),
-                ('bier_company', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='bier_shop.biercompany')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                ("image", models.ImageField(upload_to="")),
+                (
+                    "bier_type",
+                    models.CharField(
+                        choices=[
+                            ("Pilsner", "Pilsner"),
+                            ("Dark Lager", "Dark Lager"),
+                            ("German Bock", "German Bock"),
+                            ("Brown Ale", "Brown Ale"),
+                            ("Pale Ale", "Pale Ale"),
+                            ("India Pale Ale", "Indian Pale Ale"),
+                            ("Porter", "Porter"),
+                            ("Stout", "Stout"),
+                        ],
+                        max_length=100,
+                    ),
+                ),
+                ("description", models.TextField()),
+                (
+                    "bier_company",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="bier_shop.biercompany",
+                    ),
+                ),
             ],
         ),
     ]
--- my24späti/wsgi.py
+++ my24späti/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my24späti.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my24späti.settings")
 
 application = get_wsgi_application()
